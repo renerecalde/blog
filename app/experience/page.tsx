39cd257fa76e80c4bbd60513893a905f5466ee83
renerecalde <renerecalde2@gmail.com>
2024-10-20 20:02:50 -0300

    Add desciption to jobs.

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,79 +5,81 @@ export const metadata = {
     description: 'Professional experience.',
 }
 
-const people = [
+const jobs = [
     {
-        name: 'Leslie Alexander',
-        email: 'leslie.alexander@example.com',
+        cargo: 'Leslie Alexander',
+        company: 'leslie.alexander@example.com',
         role: 'Co-Founder / CEO',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
         icon: <MobileIcon/>,
     },
     {
-        name: 'Michael Foster',
-        email: 'michael.foster@example.com',
+        cargo: 'Michael Foster',
+        company: 'michael.foster@example.com',
         role: 'Co-Founder / CTO',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
         icon: <StackOverflowIcon/>,
     },
     {
-        name: 'Dries Vincent',
-        email: 'dries.vincent@example.com',
+        cargo: 'Dries Vincent',
+        company: 'dries.vincent@example.com',
         role: 'Business Relations',
         lastSeen: null,
         icon: <StackOverflowIcon/>,
     },
     {
-        name: 'Lindsay Walton',
-        email: 'lindsay.walton@example.com',
+        cargo: 'Lindsay Walton',
+        company: 'lindsay.walton@example.com',
         role: 'Front-end Developer',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
         icon: <StackOverflowIcon/>,
     },
     {
-        name: 'Courtney Henry',
-        email: 'courtney.henry@example.com',
-        role: 'Designer',
+        cargo: 'Courtney Henry',
+        company: 'courtney.henry@example.com',
+        role: 'Web Project Development.',
         lastSeen: '3h ago',
         lastSeenDateTime: '2023-01-23T13:23Z',
         icon: <StackOverflowIcon/>,
     },
     {
-        name: 'Tom Cook',
-        email: 'tom.cook@example.com',
-        role: 'Director of Product',
-        lastSeen: null,
+        cargo: 'Freelance',
+        company: 'Full time',
+        role: 'Mobile Device Programming',
+        lastSeen: 'mar. 2008 - dec. 2008 · 10 months',
         icon: <MobileIcon/>,
+        description: 'I worked in mobile device programming with J2ME. Among the systems I developed, a notable one was a voting survey system.'
     },
 ];
 
 export function Example() {
     return (
         <ul role="list" className="divide-y divide-gray-100">
-            {people.map((person) => (
-                <li key={person.email} className="flex justify-between gap-x-6 py-5">
+            {jobs.map((job) => (
+                <li key={job.company} className="flex justify-between gap-x-6 py-5">
                     <div className="flex min-w-0 gap-x-4">
                         <div className="h-12 w-12 flex-none runded-full bg-gray-10">
-                            {person.icon}
+                            {job.icon}
                         </div>
                         <div className="min-w-0 flex-auto">
-                            <p className="text-sm font-semibold leading-6 text-white-900">{person.name}</p>
-                            <p className="mt-1 truncate text-xs leading-5 text-white-500">{person.email}</p>
+                            <p className="text-sm font-semibold leading-6 text-white-900">{job.cargo}</p>
+                            <p className="mt-1 text-xs leading-5 text-white-500"> {job.description} </p>
                         </div>
                     </div>
                     <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <p className="text-sm leading-6 text-white-900">{person.role}</p>
-                        {person.lastSeen ? (
+                        <p className="text-sm leading-6 text-white-900">{job.role}</p>
+                        <p className="mt-1 truncate text-xs leading-5 text-white-500">{job.company}</p>
+                        {job.lastSeen ? (
                             <p className="mt-1 text-xs leading-5 text-white-500">
-                                Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                                Last seen <time dateTime={job.lastSeenDateTime}>{job.lastSeen}</time>
                             </p>
                         ) : (
                             <div className="mt-1 flex items-center gap-x-1.5">
                                 <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"/>
                                 </div>
                                 <p className="text-xs leading-5 text-white-500">Online</p>
                             </div>
