ba25742efb8f5110e2995878f65207a4758eb5f1
renerecalde <renerecalde2@gmail.com>
2024-10-26 12:12:03 -0300

    Finish academic

--- app/academic/page.tsx
+++ app/academic/page.tsx
@@ -1,9 +1,5 @@
 import {
     AcademicCapIcon,
-    BuildingLibraryIcon,
-    MobileIcon,
-    WebIcon,
-    BackNotesIcon, UserGroupIcon
 } from "../components/icons";
 
 export const metadata = {
@@ -13,62 +9,28 @@ export const metadata = {
 
 const titles = [
     {
-        cargo: 'Ministerio de Desarrollo Productivo de la Provincia de Santa Fe.',
-        company: 'Full time',
-        role: 'Applied Informatics Analyst.',
+        institution: 'Universidad Nacional del Litoral',
+        academicTitle: 'Computer Engineering.',
         lastSeen: null,
-        lastSeenDateTime: '2023-01-23T13:23Z',
-        icon: <BuildingLibraryIcon/>,
-        description: 'I perform web development tasks, data science, and applied informatics analyst tasks.'
-    },
-    {
-        cargo: 'COPRINF',
-        company: 'Part time',
-        role: 'Web developer',
-        lastSeen: null,
-        lastSeenDateTime: '2023-01-23T13:23Z',
-        icon: <UserGroupIcon/>,
-        description: 'Web systems development.'
-    },
-    {
-        cargo: 'Universidad Nacional del Litoral',
-        company: 'Part time',
-        role: 'Project development.',
-        lastSeen: 'feb. 2020 - mar. 2022 · 2 years 2 months',
         icon: <AcademicCapIcon/>,
-        description: 'Development of a thesis and dissertations management system.'
     },
     {
-        cargo: 'Ministerio de la Producción, Ciencia y Tecnología de la Provincia de Santa Fe.',
-        company: 'Part time',
-        role: 'Web developer contracted.',
-        lastSeen: 'sept. 2016 - mar. 2022 · 5 years and 7 months',
-        icon: <BuildingLibraryIcon/>,
-        description: 'Development of systems related to entrepreneurship and the social economy.'
+        institution: 'Universidad Nacional del Litoral',
+        academicTitle: 'Applied Informatics Analyst.',
+        lastSeen: 'may. 2021',
+        icon: <AcademicCapIcon/>,
     },
     {
-        cargo: 'Credifiar',
-        company: 'Full time',
-        role: 'Applied Informatics Analyst.',
-        lastSeen: 'jan. 2020 - mar. 2020 · 3 months',
-        icon: <BackNotesIcon/>,
-        description: 'Development tasks and general IT.'
+        institution: 'Universidad Nacional del Litoral',
+        academicTitle: 'University Bachelor\'s Degree in Engineering.',
+        lastSeen: 'may. 2015',
+        icon: <AcademicCapIcon/>,
     },
     {
-        cargo: 'Freelance',
-        company: 'Full time',
-        role: 'Web Project Development.',
-        lastSeen: 'mar. 2009 - sep. 2015 · 6 years and 7 months',
-        icon: <WebIcon/>,
-        description: 'I worked on the development of websites, mostly e-commerce platforms with Symfony for local businesses.'
-    },
-    {
-        cargo: 'Freelance',
-        company: 'Full time',
-        role: 'Mobile Device Programming',
-        lastSeen: 'mar. 2008 - dec. 2008 · 10 months',
-        icon: <MobileIcon/>,
-        description: 'I worked in mobile device programming with J2ME. Among the systems I developed, a notable one was a voting survey system.'
+        institution: 'Escuela de Educación Técnica Nº1',
+        academicTitle: 'Electrical Mechanical Technician.',
+        lastSeen: 'feb. 1995 - nov. 2001. · 6 years',
+        icon: <AcademicCapIcon/>,
     },
 ];
 
@@ -76,22 +38,20 @@ export function TitlesList() {
     return (
         <ul role="list" className="divide-y divide-gray-100">
             {titles.map((title) => (
-                <li key={title.company} className="flex justify-between gap-x-6 py-5">
+                <li key={title.institution} className="flex justify-between gap-x-6 py-5">
                     <div className="flex min-w-0 gap-x-4">
                         <div className="h-12 w-12 flex-none runded-full bg-gray-10">
                             {title.icon}
                         </div>
                         <div className="min-w-0 flex-auto">
-                            <p className="text-sm font-semibold leading-6 text-white-900">{title.cargo}</p>
-                            <p className="mt-1 text-xs leading-5 text-white-500"> {title.description} </p>
+                            <p className="text-sm font-semibold leading-6 text-white-900">{title.institution}</p>
                         </div>
                     </div>
                     <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                        <p className="text-sm leading-6 text-white-900">{title.role}</p>
-                        <p className="mt-1 truncate text-xs leading-5 text-white-500">{title.company}</p>
+                        <p className="text-sm leading-6 text-white-900">{title.academicTitle}</p>
                         {title.lastSeen ? (
                             <p className="mt-1 text-xs leading-5 text-white-500">
-                                Last seen <time dateTime={title.lastSeenDateTime}>{title.lastSeen}</time>
+                                Last seen <time>{title.lastSeen}</time>
                             </p>
                         ) : (
                             <div className="mt-1 flex items-center gap-x-1.5">
