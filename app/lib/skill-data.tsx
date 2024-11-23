06aa2984738d159a8ccb44adad5a999a46091a1b
renerecalde <renerecalde2@gmail.com>
2024-11-23 20:03:55 -0300

    add half skills data

--- app/lib/skill-data.tsx
+++ app/lib/skill-data.tsx
@@ -1,12 +1,98 @@
-import {AcademicCapIcon} from "../components/icons";
+import { DiSymfony } from "react-icons/di";
+import { SiMetabase, SiWoocommerce, SiMoodle } from "react-icons/si";
+import { RiNextjsLine } from "react-icons/ri";
+import { AiOutlinePython } from "react-icons/ai";
+import { FaPeopleArrows, FaJava } from "react-icons/fa";
+import { MdOutlineSecurity } from "react-icons/md";
+import { CiText } from "react-icons/ci";
+import { GiBrainstorm } from "react-icons/gi";
 
 const skills = [
     {
         id: 1,
-        description: 'Universidad Nacional del Litoral',
+        description: 'Web Project Development in Independent / FreelanceWeb Project Development in Independent / Freelance',
         name: 'B2B Sylium for ecommerce',
-        icon: <AcademicCapIcon/>,
-    }
+        icon: <DiSymfony size="2x"/>,
+    },
+   {
+        id: 2,
+        description: 'Project development at Universidad Nacional del LitoralDesarrollo de proyectos at Universidad Nacional del Litoral',
+        name: 'S.O.L.I.D principles',
+        icon: <DiSymfony size="2x"/>,
+    },
+    {
+        id: 3,
+        description: 'Taller WordPress Multilingüe/ FreelanceWeb Project Development in Independent',
+        name: 'Wordpress, Woocommerce',
+        icon: <SiWoocommerce size="2x"/>,
+    },
+
+    {
+        id: 4,
+        description: 'Capacitación Introducción a la Plataforma MoodleCapacitación Introducción a la Plataforma Moodle',
+        name: 'Moodle',
+        icon: <SiMoodle size="2x"/>,
+    },
+    {
+        id: 5,
+        description: 'Desarrollo de encuesta para docentes.',
+        name: 'J2me',
+        icon: <FaJava size="2x"/>,
+    },
+    {
+        id: 6,
+        description: 'Desarrollo fullstack simplificado con NextJS 13.5: App Router y Side Components',
+        name: 'NextJs',
+        icon: <RiNextjsLine size="2x"/>,
+    },
+    {
+        id: 7,
+        description: 'Desarrollo de dashboard para toma de decisiones en tiempo real para proyectos del Ministero de Desarrollo Productivo de la Provincia de Santa Fe',
+        name: 'Data Science',
+        icon: <SiMetabase size="2x"/>,
+    },
+    {
+        id: 8,
+        description: 'Cursos Metabase y Desarrollo de dashboard para toma de decisiones en tiempo real para proyectos del Ministero de Desarrollo Productivo de la Provincia de Santa Fe',
+        name: 'Metabase',
+        icon: <SiMetabase size="2x"/>,
+    },
+    {
+        id: 9,
+        description: 'Cursos UNTREF',
+        name: 'IA',
+        icon: <AiOutlinePython size="2x"/>,
+    },
+    {
+        id: 10,
+        description: 'Cursos Pentaho y Desarrollo de dashboard para toma de decisiones en tiempo real para proyectos del Ministero de Desarrollo Productivo de la Provincia de Santa Fe',
+        name: 'Pentaho',
+        icon: <SiMetabase size="2x"/>,
+    },
+    {
+        id: 11,
+        description: 'Cursos INVAP, UNL',
+        name: 'Ciberseguridad',
+        icon: <MdOutlineSecurity size="2x"/>,
+    },
+    {
+        id: 12,
+        description: 'Cursos INVAP',
+        name: 'Protección de textos',
+        icon: <CiText size="2x"/>,
+    },
+    {
+        id: 13,
+        description: 'Cursos',
+        name: 'Brainstorming',
+        icon: <GiBrainstorm size="2x"/>,
+    },
+    {
+        id: 14,
+        description: 'Cursos INVAP, GSF, UNLZ',
+        name: 'Negociación y resolución de conflictos',
+        icon: <FaPeopleArrows size="2x"/>,
+    },
 ];
 
 export function SkillsList() {
