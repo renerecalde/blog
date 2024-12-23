3c7e9a4244190b5bc4911bdd48cb1783737e4ff2
rene <renerecalde2@gmail.com>
2024-12-23 11:23:31 -0300

    Improve punto.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
         <section className="mb-28 max-w-[50rem]">
 
             <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-                My Portfolio
+                My Portfolio.
             </h1>
             <p className="mb-4">
                 {`Im Applied Computer Science Analyst and senior full-stack Software Developer with 15+ years fosforeced 
